9e1b359dd08c73f6e679d952ee55a0dbc254bda4
Nitish Kumar Jha <nitishjha913@gmail.com>
2023-04-23 15:51:54 +0530

    deploying

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const mongoose = require("mongoose");
 const dataRoutes = require("./routes/data");
 const app = express();
 const path=require("path");
+const PORT=process.env.PORT || 5000;
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -23,6 +23,6 @@ mongoose
 
 app.use("/api/data", dataRoutes);
 
-const server = app.listen(process.env.PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
